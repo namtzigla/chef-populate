270a78c578a29f1689d0fea05434b83883194872
Florin STAN <tom@tcnp3.com>
2013-10-25 14:01:55 -0600

    syntax error fixed

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,20 +3,20 @@
 # Recipe:: default
 #
 # Copyright (C) 2013 Florin STAN
-# 
+#
 # All rights reserved - Do Not Redistribute
 #
 
 
-node[:populate][:queries].each do |q| 
-	res = search(q[:index], q[:query]).map do |e| 
-		q[:fields].map do |f| 
-			if f[:append].nil?
-				e[f[:name]] 
-			else 
-			"#{e[f[:name]]}#{f[:append]}" 
-		end
-	end
-	eval "#{q[:dest]} = res" unless res != nil
+node[:populate][:queries].each do |q|
+  res = search(q[:index], q[:query]).map do |e|
+    q[:fields].map do |f|
+      if f[:append].nil?
+        e[f[:name]]
+      else
+        "#{e[f[:name]]}#{f[:append]}"
+      end
+    end    
+  end
+  eval "#{q[:dest]} = res" unless res != nil
 end
-
